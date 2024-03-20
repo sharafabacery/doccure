8acac1d2e6d162b637e973f8e6ac00eca130f280
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-03-20 15:36:16 +0200

    Create MedicalRecordController.cs

---
+++ doccure/Controllers/Doctors/MedicalRecordController.cs
@@ -0,0 +1,21 @@
+﻿using doccure.Repositories.Interfance;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace doccure.Controllers.Doctors
+{
+	[Authorize()]
+	public class MedicalRecordController : Controller
+	{
+		private readonly IMedicalRecordService medicalRecordService;
+
+		public MedicalRecordController(IMedicalRecordService medicalRecordService)
+		{
+			this.medicalRecordService = medicalRecordService;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
