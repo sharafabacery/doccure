aebf571b92cd01939aa59277980503b6edd78f8a
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-04-09 00:30:08 +0200

    Update FavouritesController.cs

--- doccure/Controllers/Doctors/FavouritesController.cs
+++ doccure/Controllers/Doctors/FavouritesController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace doccure.Controllers.Doctors
 {
+	[Authorize]
 	public class FavouritesController : Controller
 	{
 		public IActionResult Index()
