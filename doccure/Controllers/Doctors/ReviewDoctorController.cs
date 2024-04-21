0d321e6ec445d4da1537dc7d938d74b3cc2b99e3
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-04-21 21:42:56 +0200

    Doctor review

---
+++ doccure/Controllers/Doctors/ReviewDoctorController.cs
@@ -0,0 +1,28 @@
+﻿using doccure.Repositories.Interfance;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static doccure.Program;
+
+namespace doccure.Controllers.Doctors
+{
+	[Authorize(Roles ="doctor")]
+	public class ReviewDoctorController : Controller
+	{
+		private readonly IReviewService reviewService;
+		private readonly IUserProfileSettingsService userProfileSettingsService;
+
+		public ReviewDoctorController(IReviewService reviewService, ServiceResolver serviceAccessor)
+		{
+			this.reviewService = reviewService;
+			this.userProfileSettingsService = serviceAccessor("Doctor");
+		}
+		public async Task<IActionResult> Index()
+		{
+			var user = await userProfileSettingsService.GetUserData(User);
+			var reviews = await reviewService.GetAllReviews(user.doctor.Id);
+			ViewBag.User= user;
+			ViewBag.Reviews=reviews;
+			return View();
+		}
+	}
+}
