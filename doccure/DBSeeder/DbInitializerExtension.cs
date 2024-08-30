dbf296b7fb2a017104961b1e71006090acff6ce7
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-08-30 17:59:28 +0300

    Create DbInitializerExtension.cs

---
+++ doccure/DBSeeder/DbInitializerExtension.cs
@@ -0,0 +1,30 @@
+﻿using doccure.Data;
+using doccure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace doccure.DBSeeder
+{
+	internal static class DbInitializerExtension
+	{
+		public static IApplicationBuilder UseItToSeedSqlServer(this IApplicationBuilder app)
+		{
+			ArgumentNullException.ThrowIfNull(app, nameof(app));
+
+			using var scope = app.ApplicationServices.CreateScope();
+			var services = scope.ServiceProvider;
+			try
+			{
+				var context = services.GetRequiredService<ApplicationDbContext>();
+				var userMange = services.GetRequiredService<UserManager<Applicationuser>>();
+				var roleMange = services.GetRequiredService<RoleManager<IdentityRole>>();
+				DbInitializer.Initialize(context,userMange,roleMange);
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+			return app;
+		}
+	}
+}
